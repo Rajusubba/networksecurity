de84f62bc7e7b43f596745ce14ddcb448bbafd10
Raju Subba <rajusubba14@gmail.com>
2025-12-10 09:39:37 -0500

    Mlflow tracing

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation   
 from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.entity.config_entity import (
     DataIngestionConfig,
     DataValidationConfig,
     TrainingPipelineConfig,
     DataTransformationConfig,
+    ModelTrainerConfig,
 )
 from networksecurity.logging.logger import logging
 import sys
@@ -38,5 +40,12 @@ if __name__ == '__main__':
         logging.info("Data transformation completed")
         print(data_transformation_artifact)
 
+        #Model trainer
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
